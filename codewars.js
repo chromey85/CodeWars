574240ce6c23af954f13388005fe752c973a7eea
Yusuf Ayyub <yayyub85@gmail.com>
2022-07-14 23:21:44 +0100

    Growth of a Population

--- codewars.js
+++ codewars.js
@@ -235,3 +235,20 @@ console.log(getGrade(84, 79, 85));
 console.log(getGrade(75, 70, 79));
 console.log(getGrade(66, 62, 68));
 console.log(getGrade(48, 55, 52));
+
+// Growth of a Population
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+function nbYear(p0, percent, aug, p) {
+  for (let i = 0; p0 < p; i++) {
+    console.log(p0);
+    p0 = Math.floor(p0 + p0 * (percent / 100) + aug);
+    if (p0 >= p) {
+      return i + 1;
+    }
+  }
+}
+
+// console.log(nbYear(1500, 5, 100, 5000));
+// console.log(nbYear(1500000, 2.5, 10000, 2000000));
+console.log(nbYear(1000, 2, 50, 1214));
