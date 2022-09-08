808a8bfb9c5d0f50e53cd3d38dd7698704819b44
Yusuf Ayyub <yayyub85@gmail.com>
2022-09-08 17:20:50 +0100

    Find the next perfect square Kata

--- codewars.js
+++ codewars.js
@@ -306,3 +306,18 @@ console.log(updateLight("Red"));
 console.log(updateLight("Yellow"));
 console.log(updateLight("Green"));
 console.log(updateLight("Purple"));
+
+// Find the next perfect square!
+
+function findNextSquare(sq) {
+  return Number.isInteger(Math.sqrt(sq)) > 0
+    ? (Math.sqrt(sq) + 1) * (Math.sqrt(sq) + 1)
+    : -1;
+}
+
+console.log(findNextSquare(121));
+console.log(findNextSquare(625));
+console.log(findNextSquare(319225));
+console.log(findNextSquare(15241383936));
+console.log(findNextSquare(155));
+console.log(findNextSquare(342786627));
