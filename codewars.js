05c10f19e3632d1f8271d2cd17e9ea3f9a7d0661
Yusuf Ayyub <yayyub85@gmail.com>
2022-09-09 16:48:14 +0100

    Sum of two lowest positive integers Kata

--- codewars.js
+++ codewars.js
@@ -321,3 +321,18 @@ console.log(findNextSquare(319225));
 console.log(findNextSquare(15241383936));
 console.log(findNextSquare(155));
 console.log(findNextSquare(342786627));
+
+// Sum of two lowest positive integers
+
+function sumTwoSmallestNumbers(numbers) {
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+  return numbers[0] + numbers[1];
+}
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]));
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]));
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]));
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]));
