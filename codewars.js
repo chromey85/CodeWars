6282fb233037210505599d315ff6c5b3237a27d1
Yusuf Ayyub <yayyub85@gmail.com>
2022-09-16 12:13:08 +0100

    Opposites Attract kata completed

--- codewars.js
+++ codewars.js
@@ -427,3 +427,20 @@ console.log(removeExclamationMarks("Hello World!!"));
 console.log(
   removeExclamationMarks("vjmJQXtykQ!FSVfPDmlkQ xAbxfRBZdx!qIIDBgsUuK")
 );
+
+// Opposites Attract
+
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 == 0 && flower2 % 2 == 0
+    ? false
+    : flower1 % 2 !== 0 && flower2 % 2 !== 0
+    ? false
+    : true;
+}
+
+console.log(lovefunc(1, 4));
+console.log(lovefunc(2, 2));
+console.log(lovefunc(0, 1));
+console.log(lovefunc(0, 0));
+console.log(lovefunc(957, 613));
+console.log(lovefunc(7, 3));
